4cf6b7775f28bf55e51721eb32a16c75336f9a4b
David-Elkabas <david.elkabass@gmail.com>
2021-11-12 16:09:19 +0200

    last touch

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/succulent_store'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ntjlhqtozwabau:d561f2654b9bb5a7c0bd651a6ea387088961e19ee51696a83378076e097d7bc4@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/d348j0smkl266o'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ntjlhqtozwabau:d561f2654b9bb5a7c0bd651a6ea387088961e19ee51696a83378076e097d7bc4@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/d348j0smkl266o'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
