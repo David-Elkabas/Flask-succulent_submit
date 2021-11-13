8a762a0a3cb6a4958a9792dd90638ddb3efc4631
David-Elkabas <76950535+David-Elkabas@users.noreply.github.com>
2021-11-13 16:45:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/succulent_store'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'ENTER_URI_HERE'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ntjlhqtozwabau:d561f2654b9bb5a7c0bd651a6ea387088961e19ee51696a83378076e097d7bc4@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/d348j0smkl266o'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'ENTER_URI_HERE'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
